ad2c9e4e157e42bbdef02b932d7722e1b9e07862
Amritansh Pandey <amritanshpandey@Amritanshs-MacBook-Pro.local>
2025-05-05 20:45:53 +0530

    setup done

---
+++ weatherwise/src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Weather Wise",
+  description: "WeatherWise is a simple and smart weather app that provides real-time forecasts and personalized suggestions. It shows temperature, humidity, wind, and more—along with AI-powered outfit and care tips based on current conditions.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ weatherwise/src/components/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+    return (
+      <section className="p-6 md:flex md:items-center">
+    
+        <h1 className="mt-4 md:mt-0 md:ml-8 text-3xl md:text-5xl font-bold">
+          Weather that <span className="text-primary">just works</span>
+        </h1>
+      </section>
+    );
+  }
+  
